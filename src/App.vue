d692f3798da0e1d91da0be085d5d3dfeb748f14e
gds90 <you@example.com>
2024-03-01 15:12:39 +0100

    Milestone 4 Completed, able to see all projects in console

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppMain from './components/AppMain.vue';
+export default {
+  components: {
+    AppMain
+  }
+
+}
+</script>
+<template lang="">
+<AppMain />
+</template>
+<style lang="">
+  
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from '../store.js';
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            store,
+            projects: []
+        }
+    },
+    created() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios.get(`${this.store.baseUrl}/api/projects`).then((response) => {
+                this.projects = response.data.results;
+                console.log(this.projects);
+            })
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="">
+    
+</style>
