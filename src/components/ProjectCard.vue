c34bbb280deb1b52babd7ec503f08067ff00439a
gds90 <you@example.com>
2024-03-04 14:08:22 +0100

    Added technology badge in projectCard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -20,6 +20,9 @@ export default {
                 image = 'img/test-img.jpg';
             }
             return `${this.store.baseUrl}/${image}`;
+        },
+        getBadgeColor(technology) {
+            return technology.badge_color;
         }
     }
 }
@@ -32,6 +35,12 @@ export default {
             <h5 class="card-title text-center my-2">{{project.title}}</h5>
             <p class="card-text "><strong>Descrizione:</strong> {{project.description.substr(0,40)+"..."}}</p>
             <p class="card-text "><strong>Link:</strong> {{project.link.substr(0,30)+"..."}}</p>
+            <p class="card-text "><strong>Tipologie:</strong> {{project.type ? project.type.name : 'non specificato'}}</p>
+            <p class="card-text"><strong>Tecnologia:</strong>
+                <span v-for="(technology, index) in project.technology" :key="index" :class="'badge rounded-pill text-bg-' + getBadgeColor(technology)" class="ms-1">
+                    {{ technology.name }} 
+                </span>
+            </p>
         </div>
     </div>
 </div>
