294e9ece736f54db154a62dc9a52362ef260d125
gds90 <you@example.com>
2024-03-09 14:56:01 +0100

    Added personal error message

--- src/components/AppContactForm.vue
+++ src/components/AppContactForm.vue
@@ -9,11 +9,15 @@ export default {
             surname: '',
             email: '',
             phone: '',
-            content: ''
+            content: '',
+            loading: false,
+            errors: {}
         }
     },
     methods: {
         SendForm(){
+            this.loading = true;
+
             const data = {
                 name: this.name,
                 surname: this.surname,
@@ -24,8 +28,24 @@ export default {
 
             axios.post(`${this.store.baseUrl}/api/contacts`, data)
             .then(response => {
-                console.log(response.data);
-                console.log('ok');
+                console.log('Inviata');
+                if (response.data.success){
+                // Form field empty
+                this.name = '';
+                this.surname = '';
+                this.email = '';
+                this.phone = '';
+                this.content = '';
+                }
+
+                if (response.data.error) {
+                    console.log('errore');
+                    this.errors = response.data.error;
+                }
+                else {
+                    this.errors = {};
+                }
+            this.loading = false;
             });
         }
     }
@@ -37,26 +57,41 @@ export default {
             <div class="row">
                 <div class="col-6 my-3">
                     <label for="surname" class="control-label">Nome: </label>
-                    <input type="text" id="name" v-model="name" class="form-control" placeholder="Inserisci il tuo nome" required/>
+                    <input type="text" id="name" v-model="name" class="form-control" placeholder="Inserisci il tuo nome" :class="errors.name ? 'is-invalid' : ''" required/>
+                    <div v-if="errors && errors.name" class="mt-1">
+                        <p v-for="(error, index) in errors.name" :key="`message-error-$(index)`" class="text-danger">{{ error }}</p>
+                    </div>
                 </div>
                 <div class="col-6 my-3">
                     <label for="surname" class="control-label">Cognome: </label>
-                    <input type="text" id="surname" v-model="surname" class="form-control" placeholder="Inserisci il tuo cognome" required/>
+                    <input type="text" id="surname" v-model="surname" class="form-control" placeholder="Inserisci il tuo cognome" :class="errors.surname ? 'is-invalid' : ''"  required/>
+                    <div v-if="errors && errors.surname" class="mt-1">
+                        <p v-for="(error, index) in errors.surname" :key="`message-error-$(index)`" class="text-danger">{{ error }}</p>
+                    </div>
                 </div>
                 <div class="col-6">
                     <label for="email" class="control-label">Indirizzo e-mail: </label>
-                    <input type="text" id="email" v-model="email" class="form-control" placeholder="Inserisci la tua e-mail" required/>
+                    <input type="text" id="email" v-model="email" class="form-control" placeholder="Inserisci la tua e-mail" :class="errors.email ? 'is-invalid' : ''" required/>
+                    <div v-if="errors && errors.email" class="mt-1">
+                        <p v-for="(error, index) in errors.email" :key="`message-error-$(index)`" class="text-danger">{{ error }}</p>
+                    </div>
                 </div>
                 <div class="col-6">
                     <label for="phone" class="control-label">Telefono: </label>
-                    <input type="text" id="phone" v-model="phone" class="form-control" placeholder="Inserisci il tuo numero" required/>
+                    <input type="text" id="phone" v-model="phone" class="form-control" placeholder="Inserisci il tuo numero" :class="errors.phone ? 'is-invalid' : ''" required/>
+                    <div v-if="errors && errors.phone">
+                        <p v-for="(error, index) in errors.phone" :key="`message-error-$(index)`" class="text-danger">{{ error }}</p>
+                    </div>
                 </div>
                 <div class="col-12 my-3">
                     <label for="content" class="control-label">Messaggio: </label>
-                    <textarea name="content" id="content" rows="4" class="form-control" v-model="content" placeholder="Inserisci il tuo messaggio"></textarea>
+                    <textarea name="content" id="content" rows="4" class="form-control" v-model="content" placeholder="Inserisci il tuo messaggio"  :class="errors.content ? 'is-invalid' : ''"  required></textarea>
+                    <div v-if="errors && errors.content" class="mt-1">
+                        <p v-for="(error, index) in errors.content" :key="`message-error-$(index)`" class="text-danger">{{ error }}</p>
+                    </div>
                 </div>
                 <div class="col-12 mt-2">
-                    <button  type="submit" class="btn btn-primary float-right">Invia</button>
+                    <button  type="submit" class="btn btn-primary float-right" :disabled="loading">{{loading ? 'Invio in corso..' : 'Invia'}}</button>
                 </div> 
             </div>
         </form>
